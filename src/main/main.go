33cfbbd4757ce8dba92e08390b7ec6e960103e58
allen <silence4allen@gmail.com>
2018-07-31 11:08:41 +0800

    add home 3th

--- src/main/main.go
+++ src/main/main.go
@@ -1,12 +1,15 @@
 package main
 
+import "fmt"
+
 func main() {
 	test()
 }
 
 func test() {
-	for i:=1;i ;  {
-
+	for i := 1; i < 10; i++ {
+		fmt.Println(i)
 	}
 	//add home
+	//add home main 3th
 }
