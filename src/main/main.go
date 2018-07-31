36754d32bdd7b6ff8718ec9404651ace5ef51187
allen <silence4allen@gmail.com>
2018-08-01 00:20:59 +0800

    add demo go file error_returnval mult_returnval varnumpar

--- src/main/main.go
+++ src/main/main.go
@@ -3,14 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	test()
-}
+	a := 5
+	p := &a
+	*p = 7
 
-func test() {
-	for i := 1; i < 10; i++ {
-		fmt.Println(i)
+	fmt.Printf("p代表的是a的地址：%x\n", p)
+	fmt.Printf("&a代表的是a的地址：%x\n", &a)
+	fmt.Printf("*p代表的是a的值：%d\n", *p)
+	fmt.Printf("a代表的是a的值：%d\n", a)
+	fmt.Printf("&p代表的是p的地址：%x\n", &p)
+
+	s := []int{7, 9, 3, 5, 1}
+	for index, content := range s {
+		fmt.Printf("index is %d 的为：", index)
+		fmt.Println(content)
 	}
-	//add home
-	//add home main 3th
-	//add pc-own
 }
