3f86eb9c983d2a34fd72869021699012d2daf074
allen <silence4allen@gmail.com>
2018-08-02 23:48:50 +0800

    change fibonacci

--- src/main/main.go
+++ src/main/main.go
@@ -1,21 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+	"GoTest/src/demo"
+)
 
 func main() {
-	a := 5
-	p := &a
-	*p = 7
+	start := time.Now()
 
-	fmt.Printf("p代表的是a的地址：%x\n", p)
-	fmt.Printf("&a代表的是a的地址：%x\n", &a)
-	fmt.Printf("*p代表的是a的值：%d\n", *p)
-	fmt.Printf("a代表的是a的值：%d\n", a)
-	fmt.Printf("&p代表的是p的地址：%x\n", &p)
+	demo.TestFibo3()
 
-	s := []int{7, 9, 3, 5, 1}
-	for index, content := range s {
-		fmt.Printf("index is %d 的为：", index)
-		fmt.Println(content)
-	}
+	end := time.Now()
+	a := end.Sub(start)
+	fmt.Printf("It takes %v ", a)
 }
